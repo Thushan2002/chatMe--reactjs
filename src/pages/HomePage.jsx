5487f0ec0e6db768c7c6fa78e257397839eccb97
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-07-06 21:32:00 +0530

    feat: HomePage setup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import ChatContainer from "../components/ChatContainer";
+import RightSidebar from "../components/RightSidebar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [selectedChat, setSelectedChat] = useState(null);
+
+  return (
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-500 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${
+          selectedChat ? "md:grid-cols-3" : "md:grid-cols-2"
+        }`}>
+        <Sidebar />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
