14765c31e900d2fba33626d5f64566f714aa430b
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-07-07 21:39:41 +0530

    feat:chat container added

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-import assets from "../assets/assets";
+import React, { useEffect, useRef } from "react";
+import assets, { messagesDummyData } from "../assets/assets";
+import { formatMessageTime } from "../lib/util";
 
 const ChatContainer = ({ selectedChat, setSelectedChat }) => {
+  const scrollEnd = useRef();
+  // Scroll to the bottom of the chat when the component mounts
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return selectedChat ? (
     <div className="h-full overflow-scroll relative backdrop-blur-2xl">
       <div className="flex items-center py-3 mx-4 border-gray-100 border-b">
@@ -17,6 +26,63 @@ const ChatContainer = ({ selectedChat, setSelectedChat }) => {
           className="md:hidden max-w-7"
         />
         <img src={assets.help_icon} alt="" className="max-md:hidden max-w-5" />
+      </div>
+      {/* chat area */}
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6">
+        {messagesDummyData.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex items-end gap-2 justify-end ${
+              msg.senderId !== "680f50e4f10f3cd28382ecf9" && "flex-row-reverse"
+            }`}>
+            {msg.image ? (
+              <img
+                src={msg.image}
+                className="max-w-[230px] border border-primary rounded-lg overflow-hidden mb-8"
+              />
+            ) : (
+              <p
+                className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${
+                  msg.senderId !== "680f50e4f10f3cd28382ecf9"
+                    ? "rounded-bl-none"
+                    : "rounded-br-none"
+                }`}>
+                {msg.text}
+              </p>
+            )}
+            <div className="text-center text-xs">
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                alt=""
+                className="w-6 rounded-full mb-1"
+              />
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}></div>
+      </div>
+
+      {/* bottom area */}
+      <div className="absolute bottom-0 left-0 right-0 p-3 flex items-center bg-primary-light/5 ">
+        <div className="flex-1 flex items-center bg-white rounded-full px-3 py-2 shadow-md">
+          <input type="text" placeholder="Write a message" />
+          <input type="file" id="image" accept="image/png image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              alt=""
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+          <img src={assets.send_button} alt="" className="w-7 cursor-pointer" />
+        </div>
       </div>
     </div>
   ) : (
---
+++ src/lib/util.js
@@ -0,0 +1,8 @@
+export const formatMessageTime = (date) => {
+    const options = {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    };
+    return new Date(date).toLocaleTimeString("en-US", options);
+}
