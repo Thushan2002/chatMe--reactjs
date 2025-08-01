d6f4ff388f166d3f2b2dd62a32b6bf274dc118c7
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-08-01 13:56:03 +0530

    chore: cors origin changed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app)
 
 // middleware setup 
 app.use(express.json({ limit: "20mb" }))
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: true, credentials: true }))
 
 // initialise socket.io server
 
